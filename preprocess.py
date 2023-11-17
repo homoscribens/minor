72ebb8bd3f9ca0eafc4f9c19c5eee70f5eab95a9
scribens-home <dash400air@gmail.com>
2023-11-17 17:13:54 +0900

    [update ]Refactor preprocess_eaf function and add preprocess_dump function

--- preprocess.py
+++ preprocess.py
@@ -1,11 +1,14 @@
-import json
 import pathlib
 from xml.etree import ElementTree as ET
 
+import pandas as pd
 from tqdm import tqdm
 
 
-def main(eaf_content, output_dir):
+def preprocess_eaf(eaf_path):
+    with open(eaf_path, "r", encoding="utf-8") as eaf_file:
+        eaf_content = eaf_file.read()
+
     root = ET.fromstring(eaf_content)
 
     # Re-extracting time slots
@@ -24,11 +27,19 @@ def main(eaf_content, output_dir):
         for annotation in tier.findall(".//ANNOTATION"):
             ann_value = annotation.find(".//ANNOTATION_VALUE")
             ref_time_slot = annotation.find(".//ALIGNABLE_ANNOTATION")
-            if ann_value is not None and ann_value.text and ref_time_slot is not None:
+            if (
+                ann_value is not None
+                and ann_value.text
+                and ref_time_slot is not None
+            ):
                 annotation_detail = {
                     "utterance": ann_value.text,
-                    "start_time": int(time_slots[ref_time_slot.get("TIME_SLOT_REF1")]),
-                    "end_time": int(time_slots[ref_time_slot.get("TIME_SLOT_REF2")]),
+                    "start_time": int(
+                        time_slots[ref_time_slot.get("TIME_SLOT_REF1")]
+                    ),
+                    "end_time": int(
+                        time_slots[ref_time_slot.get("TIME_SLOT_REF2")]
+                    ),
                 }
                 if "user_utterance" in tier_id.lower():
                     user_utterances.append(annotation_detail)
@@ -59,12 +70,14 @@ def main(eaf_content, output_dir):
         time_key = (utterance["start_time"], utterance["end_time"])
         if time_key not in grouped_utterances:
             grouped_utterances[time_key] = {"System": None, "User": None}
-        grouped_utterances[time_key][utterance["type"]] = utterance["utterance"]
+        grouped_utterances[time_key][utterance["type"]] = utterance[
+            "utterance"
+        ]
 
     # Convert the grouped utterances to a list format for JSON
-    json_output = []
+    eaf_output = []
     for time_key, utterances in grouped_utterances.items():
-        json_output.append(
+        eaf_output.append(
             {
                 "start_time": time_key[0],
                 "end_time": time_key[1],
@@ -73,19 +86,50 @@ def main(eaf_content, output_dir):
             }
         )
 
-    # Sorting the JSON data based on start_time
-    sorted_json_output = sorted(json_output, key=lambda x: x["start_time"])
+    # Sorting the data based on start_time
+    sorted_eaf_output = sorted(eaf_output, key=lambda x: x["start_time"])
 
-    # Writing the sorted data to a new JSON file
-    output_sorted_json_path = output_dir / (eaf_path.stem + ".json")
-    with open(output_sorted_json_path, "w", encoding="utf-8") as json_file:
-        json.dump(sorted_json_output, json_file, ensure_ascii=False, indent=4)
+    return sorted_eaf_output
+
+
+def preprocess_dump(eaf_path, sorted_eaf_output):
+    dump_path = pathlib.Path("./dumpfiles")
+    dump_file = dump_path / (eaf_path.stem + ".csv")
+
+    if not dump_file.exists():
+        return None
+    dump_df = pd.read_csv(dump_file).filter(regex=r"^(?!word#)")
+
+    user = []
+    system = []
+    for i, dump_start_time in enumerate(dump_df["start(exchange)[ms]"]):
+        for eaf in sorted_eaf_output:
+            eaf_start_time = eaf["start_time"]
+            if dump_start_time - eaf_start_time < 5:
+                user.append(eaf["User"])
+                system.append(eaf["System"])
+                break
+
+    assert len(dump_df) == len(system)
+
+    dump_df["system_utterance"] = system
+    dump_df["user_utterance"] = user
+
+    return dump_df
+
+
+def main(eaf_path, output_dir):
+    preprocessed_data = preprocess_eaf(eaf_path)
+    dump_df = preprocess_dump(eaf_path, preprocessed_data)
+
+    if dump_df is not None:
+        dump_df.to_csv(output_dir / (eaf_path.stem + ".csv"), index=False)
 
 
 if __name__ == "__main__":
     data_dir = pathlib.Path("./elan")
     output_dir = pathlib.Path("./preprocessed")
+    if not output_dir.exists():
+        output_dir.mkdir()
     for eaf_path in tqdm(data_dir.glob("*.eaf")):
-        with open(eaf_path, "r", encoding="utf-8") as eaf_file:
-            eaf_content = eaf_file.read()
-        main(eaf_content, output_dir)
+        main(eaf_path, output_dir)
